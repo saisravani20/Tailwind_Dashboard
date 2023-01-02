16906a531708ea97bdcdbf1e76beab582affab18
saisravani20 <saisravani.koppala@gmail.com>
2023-01-02 10:02:13 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/index.html"],
+  theme: {
+    extend: {
+      
+    },
+    colors: {
+      'icon-color': '#ff0000',
+      clr: '#ff0000',
+      
+      
+    },
+  },
+  variants: {},
+  plugins: [],
+}
+
